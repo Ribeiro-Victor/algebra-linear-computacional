0a84917fb30d0b53ae8684b6e0817f45b157933d
Victor Ribeiro <ribeir145@gmail.com>
2022-05-13 14:15:52 -0300

    Adiciona decomposição LU

--- task_01.py
+++ task_01.py
@@ -1,18 +1,43 @@
-from utils import read_matrix, print_matrix
+from utils import read_matrix, print_matrix, matrix_determinant
+import copy
 
-n = int(input("Entre com a ordem do sistema de equacoes: "))
-print("""
-Decomposição LU (ICOD = 1)
-Decomposição de Cholesky (ICOD = 2)
-Procedimento iterativo Jacobi (ICOD = 3)
-Procedimento iterativo Gauss-Seidel (ICOD = 4).
-""")
-icod = int(input("Entre com o ICOD da operacao: "))
-idet = int(input("Entre com o IDET (IDET = 0 não calcula determinante ou maior que 0 calcula o determinante): "))
-matrix_A = read_matrix("entrada_A.txt")
-vector_B = read_matrix("entrada_B.txt")
-if(icod == 3 or icod == 4):
-    TOLm = float(input("Entre com a tolerancia maxima para solucao iterativa: "))
+def lu_decomposition(matrix):
+
+    number_of_rows = len(matrix)
+    number_of_columns = len(matrix[0])
+
+    if(number_of_columns != number_of_rows):
+        return("A matriz deve ser quadrada para realizar este metodo.")
+
+    #if(matrix_determinant(matrix)==0):
+    #    return("A matriz deve ser não singular para realizar este metodo.")
+    
+    result = copy.deepcopy(matrix)
+
+    for k in range(number_of_columns):
+        for i in range(k+1, number_of_columns):
+            result[i][k] = result[i][k]/result[k][k]
+
+        for j in range(k+1, number_of_columns):
+            for i in range(k+1, number_of_columns):
+                result[i][j] = result[i][j]-result[i][k]*result[k][j]
+    
+    return result
+
+if __name__ == "__main__":
+    n = int(input("Entre com a ordem do sistema de equacoes: "))
+    print("""
+    Decomposição LU (ICOD = 1)
+    Decomposição de Cholesky (ICOD = 2)
+    Procedimento iterativo Jacobi (ICOD = 3)
+    Procedimento iterativo Gauss-Seidel (ICOD = 4).
+    """)
+    icod = int(input("Entre com o ICOD da operacao: "))
+    idet = int(input("Entre com o IDET (IDET = 0 não calcula determinante ou maior que 0 calcula o determinante): "))
+    matrix_A = read_matrix("entrada_A.txt")
+    vector_B = read_matrix("entrada_B.txt")
+    if(icod == 3 or icod == 4):
+        TOLm = float(input("Entre com a tolerancia maxima para solucao iterativa: "))
 
 
 
