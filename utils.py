53b24c91abb68f0029fd37f6ea5f57b32d91d013
Victor Ribeiro <ribeir145@gmail.com>
2022-05-13 17:15:25 -0300

    Adiciona a substituição pra frente e pra tras

--- utils.py
+++ utils.py
@@ -5,9 +5,17 @@ def read_matrix(file_path):
         matrix = [[float(num) for num in row.split(' ')] for row in f]
     return matrix
 
+def read_vector(file_path):
+    with open(file_path, 'r') as f:
+        vector = [float(num) for num in f]
+    return vector
+
 def print_matrix(matrix):
     print('\n'.join([''.join(['{:10.2f}'.format(item) for item in row]) 
       for row in matrix]))
+    
+def print_vector(vector):
+    print('\n'.join('{:10.2f}'.format(item) for item in vector))
 
 def matrix_determinant(matrix):
     #Calcula determinante a partir da expansao de cofatores
@@ -51,3 +59,44 @@ def check_symmetry(matrix):
                 return False
     
     return True
+
+def forward_substitution(matrix_l, vector_b, flag=True):
+    #Flag utilizada para indicar que a matrix_l é diagonal inferior
+    #mas sua diagonal está "suja" com outros termos da matriz A
+    
+    number_of_rows = len(matrix_l)
+    number_of_columns = len(matrix_l[0])
+    vector_x = [0.0] * number_of_columns
+
+    if(flag):
+        vector_x[0] = vector_b[0]
+    else:
+        vector_x[0] = vector_b[0]/matrix_l[0][0]
+
+    for i in range(1, number_of_rows):
+        summation = sum(matrix_l[i][j] * vector_x[j] for j in range(i))
+
+        if(flag):
+            vector_x[i] = (vector_b[i] - summation)
+        else:
+            vector_x[i] = (vector_b[i] - summation)/matrix_l[i][i]
+    
+    return vector_x
+
+def backward_substitution(matrix_u, vector_b):
+    
+    number_of_rows = len(matrix_u)
+    number_of_columns = len(matrix_u[0])
+    vector_x = [0.0] * number_of_columns
+
+    vector_x[number_of_columns-1] = vector_b[number_of_columns-1]/matrix_u[number_of_rows-1][number_of_columns-1]
+
+    for i in range(number_of_rows-2, -1, -1):
+        summation = sum(matrix_u[i][j] * vector_x[j] for j in range(i+1, number_of_columns))
+
+        vector_x[i] = (vector_b[i] - summation)/matrix_u[i][i]
+    
+    return vector_x
+
+
+
