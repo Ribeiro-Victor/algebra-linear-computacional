99f5affcfecff0c9d54c1d715231b6945aab9f8a
Victor Ribeiro <ribeir145@gmail.com>
2022-05-14 20:58:18 -0300

    Adiciona metodo gauss-seidel

--- task_01.py
+++ task_01.py
@@ -1,8 +1,9 @@
 from decimal import DivisionByZero
 from tabnanny import check
-from utils import backward_substitution, diagonally_dominant, print_vector, check_symmetry, forward_substitution, read_matrix, print_matrix, matrix_determinant, transpose_matrix, vector_euclidean_norm, vector_subtraction
+
+from numpy import isin, matrix
+from utils import backward_substitution, diagonally_dominant, print_vector, check_symmetry, forward_substitution, read_matrix, print_matrix, matrix_determinant, read_vector, transpose_matrix, vector_euclidean_norm, vector_subtraction
 import copy
-import math
 
 def lu_decomposition(matrix):
 
@@ -42,8 +43,8 @@ def cholesky_decomposition(matrix):
     if(not check_symmetry(matrix)):
         return "ERRO: A matriz deve ser simétrica para realizar este método."
 
-    result = copy.deepcopy(matrix)
-    #result = matrix
+    #result = copy.deepcopy(matrix)
+    result = matrix
 
     for i in range(number_of_rows):
         summation = sum(result[i][k]**2 for k in range(i))
@@ -91,6 +92,8 @@ def iterative_jacobi(matrix_a, vector_b, tol):
     residue = 1
     iteration = 0
 
+    result = [[copy.deepcopy(initial_solution), residue]]
+
     while(residue > tol):
 
         for i in range(len(next_solution)):
@@ -100,22 +103,58 @@ def iterative_jacobi(matrix_a, vector_b, tol):
                 if(j!=i):
                     summation += (-1)*matrix_a[i][j]*initial_solution[j]
     
-            next_solution[i] = (vector_b[i] + summation)/matrix_a[i][i]
+            next_solution[i] = round((vector_b[i] + summation)/matrix_a[i][i], 3)
 
+        iteration += 1
         residue = vector_euclidean_norm(vector_subtraction(next_solution, initial_solution))\
                 /vector_euclidean_norm(next_solution)
         
         for i in range(len(next_solution)):
             initial_solution[i] = next_solution[i]
         
+        result.append([copy.deepcopy(next_solution), residue])
+
+    return result
+
+def iterative_gauss_seidel(matrix_a, vector_b, tol):
+
+    if(not diagonally_dominant(matrix_a)):
+        return "ERROR: Solução não converge, matriz não é diagonal dominante."
+
+    initial_solution = [1.0] * len(vector_b)
+    next_solution = [0.0] * len(vector_b)
+
+    residue = 1
+    iteration = 0
+
+    result = [[copy.deepcopy(initial_solution), residue]]
+
+    while(residue > tol):
+
+        for i in range(len(next_solution)):
+            summation = 0.0
+
+            for j in range(len(next_solution)):
+                if(j>i):
+                    summation += (-1)*matrix_a[i][j]*initial_solution[j]
+                if(j<i):
+                    summation += (-1)*matrix_a[i][j]*next_solution[j]
+    
+            next_solution[i] = round((vector_b[i] + summation)/matrix_a[i][i],3)
+
         iteration += 1
+        residue = vector_euclidean_norm(vector_subtraction(next_solution, initial_solution))\
+                /vector_euclidean_norm(next_solution)
+        
+        for i in range(len(next_solution)):
+            initial_solution[i] = next_solution[i]
+        
+        result.append([copy.deepcopy(next_solution), residue])
 
-    print_vector(next_solution)
-    print("iteration: ", iteration)
-    print("residue: ", "{:.2e}".format(residue))
-
+    return result
+        
 if __name__ == "__main__":
-    n = int(input("Entre com a ordem do sistema de equacoes: "))
+    #n = int(input("Entre com a ordem do sistema de equacoes: "))
     print("""
     Decomposição LU (ICOD = 1)
     Decomposição de Cholesky (ICOD = 2)
@@ -124,10 +163,50 @@ if __name__ == "__main__":
     """)
     icod = int(input("Entre com o ICOD da operacao: "))
     idet = int(input("Entre com o IDET (IDET = 0 não calcula determinante ou maior que 0 calcula o determinante): "))
-    matrix_A = read_matrix("entrada_A.txt")
-    vector_B = read_matrix("entrada_B.txt")
+    matrix_A = read_matrix("matrix5.txt")
+    vector_B = read_vector("vector5.txt")
+    if(icod == 1):
+        x = solve_system(matrix_A, vector_B, use_lu_method=True)
+        if(not isinstance(x, str)):
+            print_vector(x)
+            if(idet > 0):
+                det = matrix_A[0][0]
+                for i in range(1, len(matrix_A)):
+                    det = det * matrix_A[i][i]
+                print("Determinante: ", det)
+        else:
+            print(x)
+    if(icod == 2):
+        x = solve_system(matrix_A, vector_B, use_lu_method=False)
+        if(not isinstance(x, str)):
+            print_vector(x)
+            if(idet > 0):
+                det = matrix_A[0][0]
+                for i in range(1, len(matrix_A)):
+                    det = det * matrix_A[i][i]
+                det = det**2
+                print("Determinante: ", det)
+        else:
+            print(x)
+
+
     if(icod == 3 or icod == 4):
         TOLm = float(input("Entre com a tolerancia maxima para solucao iterativa: "))
+        if(icod == 3):
+            result = iterative_jacobi(matrix_A, vector_B, TOLm)
+        else:
+            result = iterative_gauss_seidel(matrix_A, vector_B, TOLm)
+        if(not isinstance(result, str)):
+            for i in range(1, len(result)):
+                print("Iteração: ", i)
+                print("Vetor x_i  : ", result[i-1][0])
+                print("Vetor x_i+1: ", result[i][0])
+                print("Residuo: ", "{:.2e}".format(result[i][1]), "\n")
+        else:
+            print(result)
+
+    
+
 
 
 
