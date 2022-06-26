f76c3c2ebdc242e8303140cfd21d44be80f77c01
Victor Ribeiro <ribeir145@gmail.com>
2022-06-26 11:08:45 -0300

    Fix: padroniza entradas p2_task2

--- P2/task02/task02.py
+++ P2/task02/task02.py
@@ -22,6 +22,7 @@ def run():
         n = int(parser['INPUT']['n'])
         delta_x1 = float(parser['INPUT']['delta_x1'])
         delta_x2 = float(parser['INPUT']['delta_x2'])
+        output_path = parser['INPUT']['output']
         if(n<2 or n>10):
             print("ERROR: 'n' deve estar entre 2 e 10.")
             return 
@@ -31,19 +32,29 @@ def run():
     except:
         raise Exception("ERROR: Arquivo de input com erro.")
 
-    buffer = ''
+    buffer = '-'*20 + 'Dados lidos' + '-'*20 + '\n'
+    buffer += f'ICOD = {icod}\n'
+    buffer += f'c1 = {c1}\tc2 = {c2}\tc3 = {c3}\tc4 = {c4}\n'
     if(icod == 1):
         solution = Function_Root(c1, c2, c3, c4)
+        buffer += f'a = {a}\tb = {b}\n'
+        buffer += f'TOLm = {maxTolerance}\n'
+        buffer += '-'*22 + 'Solucao' + '-'*22 + '\n'
         buffer += f'Raiz da funcao pelo metodo da bissecao:\t{solution.bissection_method(a, b, maxTolerance)}\n'
         buffer += f'Raiz da funcao pelo metodo de newton:\t{solution.newton_method(a, b, maxTolerance)}\n'
     
     elif(icod == 2):
-        solution = Integration(-1, 1, 10, 1)
-        buffer += f'Integral por Quadratura Polinomial:\t{solution.polynomial_quadrature(a, b, n)}\n'
+        solution = Integration(c1, c2, c3, c4)
+        buffer += f'a = {a}\tb = {b}\n'
+        buffer += f'num de pontos de integracao = {n}\n'
+        buffer += '-'*22 + 'Solucao' + '-'*22 + '\n'
+        buffer += f'Integral por Quadratura Polinomial:\t\t{solution.polynomial_quadrature(a, b, n)}\n'
         buffer += f'Integral por Quadratura Gauss-Legendre:\t{solution.legendre_gauss_quadrature(a, b, n)}\n'
 
     elif(icod == 3):
         solution = Derivative(c1, c2, c3, c4)
+        buffer += f'a = {a}\tdelta_x = {delta_x1}\n'
+        buffer += '-'*22 + 'Solucao' + '-'*22 + '\n'
         #buffer += f'Derivada analítica:\t\t {Function_Root(c1, c2, c3, c4).get_derivative_value(a)}\n'
         buffer += f'Diferenca finita passo a frente:\t{solution.forward_finite_difference(a, delta_x1)}\n'
         buffer += f'Diferenca finita passo atras:\t\t{solution.backward_finite_difference(a, delta_x1)}\n'
@@ -51,31 +62,13 @@ def run():
     
     else:
         solution = Derivative(c1, c2, c3, c4)
+        buffer += f'a = {a}\tdelta_x1 = {delta_x1}\tdelta_x2 = {delta_x2}\n'
+        buffer += '-'*22 + 'Solucao' + '-'*22 + '\n'
         buffer += f'Extrapolacao de Richard: {solution.richard_extrapolation(a, delta_x1, delta_x2)}\n'
 
-    with open("P2/task02/output.txt", "w") as file:
+    with open("P2/task02/"+output_path, "w") as file:
         file.write(buffer)
     
-    print("P2 - Task02 executada com sucesso. Saída disponível em: P2/task02/output")
-
-
-
-    
-    
-#run()
-
-if(False):
-    r = Function_Root(-1, 1, 10, 1)
-    print(r.bissection_method(2, 5, 0.0000000001))
-    print(r.newton_method(2, 5, 0.0000000001))
-
-    d = Derivative(-1, 1, 10, 1)
-    print("Derivada analítica: ", r.get_derivative_value(2))
-    delta1 = 0.01
-    delta2 = 0.05
-    print("Diferenca finita para frente: ",d.forward_finite_difference(2, delta1))
-    print("Diferenca finita para tras: ",d.backward_finite_difference(2, delta1))
-    print("Diferenca finita central: ",d.central_finite_difference(2, delta1))
-    print("Extrapolacao de richard: ",d.richard_extrapolation(2, delta1, delta2))
+    print("P2 - Task02 executada com sucesso. Saída disponível em P2/task02/"+output_path)
 
 
